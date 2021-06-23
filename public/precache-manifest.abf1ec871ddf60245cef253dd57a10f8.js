04bd0aadb11137eb356036be793ce649f76ea376
Franco Peralta <franco.peralta7@hotmail.com>
2021-06-23 20:19:51 -0300

    v1 CalendarApp

---
+++ public/precache-manifest.abf1ec871ddf60245cef253dd57a10f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baf59c1853d3fc0016118a05c31f1818",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2be79f90bcc8d65d7506",
+    "url": "/static/css/2.59a0ab78.chunk.css"
+  },
+  {
+    "revision": "24a7527012c495e8c72c",
+    "url": "/static/css/main.c1926377.chunk.css"
+  },
+  {
+    "revision": "2be79f90bcc8d65d7506",
+    "url": "/static/js/2.bd64b146.chunk.js"
+  },
+  {
+    "revision": "fa4b4056bba234820139274405351863",
+    "url": "/static/js/2.bd64b146.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24a7527012c495e8c72c",
+    "url": "/static/js/main.bb258c98.chunk.js"
+  },
+  {
+    "revision": "c2ab9c45931d577cf16e",
+    "url": "/static/js/runtime-main.27b01e2e.js"
+  }
+]);
